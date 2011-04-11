5586bf2354c9e4f270c301ff9a9308beb78f6415
Xiang Zhao <xiang.umass@gmail.com>
2011-04-11 17:05:54 -0400

    Test on Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,10 @@
  *  Created on: Apr 11, 2011
  *      Author: Xiang Zhao
  */
+#include <iostream>
+using namespace std;
 int main() {
+	char* pch = "Hello";
+	cout << *(pch + 1) << endl;
 	return 0;
 }

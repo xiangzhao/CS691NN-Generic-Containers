4f0957438b18873a6ba4cbe649947891298ab39a
Eli Gottlieb <eligottlieb@gmail.com>
2011-04-13 16:01:01 -0400

    Working on sequences.

---
+++ adaptivity.h
@@ -0,0 +1,13 @@
+/*
+ * adaptivity.h
+ *
+ *  Created on: Apr 13, 2011
+ *      Authors: Xiang Zhao, Eli Gottlieb
+ */
+
+#ifndef ADAPTIVITY_H
+#define ADAPTIVITY_H
+
+enum complexity_class_t { CONSTANT, LINEAR, NLOGN };
+
+#endif 

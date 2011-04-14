3243eb8a9b82d8a61466dcff805bd0d03196e81c
Xiang Zhao <xiang.umass@gmail.com>
2011-04-14 16:42:02 -0400

    BUILD Files

---
+++ subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../AdaptiveKeyValuePairs.cpp \
+../AdaptiveMultiSet.cpp \
+../AdaptiveSequence.cpp \
+../Main.cpp 
+
+OBJS += \
+./AdaptiveKeyValuePairs.o \
+./AdaptiveMultiSet.o \
+./AdaptiveSequence.o \
+./Main.o 
+
+CPP_DEPS += \
+./AdaptiveKeyValuePairs.d \
+./AdaptiveMultiSet.d \
+./AdaptiveSequence.d \
+./Main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+

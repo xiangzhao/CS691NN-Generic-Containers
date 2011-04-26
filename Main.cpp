0f05e296f30e3e783ce8e21c5912216f0bc6cac0
Xiang Zhao <xiang.umass@gmail.com>
2011-04-26 14:57:28 -0400

    Fix bugs of initialization of representation tag. Add iterator operations.

--- Main.cpp
+++ Main.cpp
@@ -5,6 +5,8 @@
  *      Author: Xiang Zhao
  */
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include "AdaptiveSequence.h"
 int main() {
 	//	char* pch = "Hello";
@@ -12,6 +14,24 @@ int main() {
 	AdaptiveSequence<int> ads;
 	ads.push_back(5);
 	ads.push_back(6);
-	AdaptiveSequence<int>::ite iterator = ads.begin();
+	ads.push_back(7);
+	ads.push_back(8);
+
+	//	AdaptiveSequence<int>::ite iterator = ads.begin();
+	//	AdaptiveSequence<int>::ite iteratorend = ads.end();
+	//	std::vector<int>::iterator resultite = std::find(
+	//			iterator.vector_random_access_iterator,
+	//			iteratorend.vector_random_access_iterator, 6);
+	for (AdaptiveSequence<int>::ite iterator = ads.begin(); iterator
+			!= ads.end(); ++iterator) {
+		std::cout << *iterator << std::endl;
+	}
+	//	std::cout << *resultite << std::endl;
+	//	std::cout << *iterator << std::endl;
+	//	ads.getInternals();
+	//	std::cout << *(++iterator) << std::endl;
+	//	std::cout << *(++iterator) << std::endl;
+	//	std::cout << *--iteratorend << std::endl;
+
 	return 0;
 }
